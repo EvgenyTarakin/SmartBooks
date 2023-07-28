7d117c5d54d19a5fe7dd05193f9745a424bd97f7
Evgeny Tarakin <luzju99@mail.ru>
2023-07-29 00:06:02 +0700

    Initial Commit

---
+++ SmartBooks/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewControllerExtension.swift
+//  SmartBooks
+//
+//  Created by Евгений Таракин on 29.07.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setupDefaultSettings() {
+        view.backgroundColor = .white
+    }
+}
---
+++ SmartBooks/MainApplication/Readers/ReadersViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReadersViewController.swift
+//  SmartBooks
+//
+//  Created by Евгений Таракин on 28.07.2023.
+//
+
+import UIKit
+
+final class ReadersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupDefaultSettings()
+    }
+
+
+}
+
